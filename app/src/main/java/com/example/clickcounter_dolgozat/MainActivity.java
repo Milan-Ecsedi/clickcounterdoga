122918d327514a3eeeef82138aa122aed688cb54
Ecsedi Milan <milan200314@gmail.com>
2022-09-23 12:01:55 +0200

    main.java   -tud adni-vonni a számhoz   -megváltoztatja a színét a számnak attól függően hogy pozitív vagy negatív

--- app/src/main/java/com/example/clickcounter_dolgozat/MainActivity.java
+++ app/src/main/java/com/example/clickcounter_dolgozat/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.clickcounter_dolgozat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private Button ButtonHozzaad;
     private Button ButtonKivon;
     private TextView Kimenet;
+    private int Szam;
 
 
     @Override
@@ -26,11 +28,56 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-               String Szam = String.valueOf(Kimenet);
+
                 Szam  += 1;
-                Kimenet.setText(Szam);
+                Kimenet.setText(String.valueOf(Szam));
+                if (Szam > 0){
+
+                    Kimenet.setTextColor(Color.rgb(13,239,81));
+
+                }
+                else if(Szam<0){
+
+                    Kimenet.setTextColor(Color.rgb(239,13,13));
+
+                }
+                else {
+
+                    Kimenet.setTextColor(Color.rgb(13,13,239));
+
+                }
 
 
+            }
+        });
+
+
+
+
+
+        ButtonKivon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Szam  -= 1;
+                Kimenet.setText(String.valueOf(Szam));
+
+                if (Szam > 0){
+
+                    Kimenet.setTextColor(Color.rgb(13,239,81));
+
+                }
+                else if(Szam<0){
+
+                    Kimenet.setTextColor(Color.rgb(239,13,13));
+
+                }
+                else {
+
+                    Kimenet.setTextColor(Color.rgb(13,13,239));
+
+                }
 
             }
         });
@@ -44,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
         ButtonHozzaad= findViewById(R.id.ButtonPlus);
         ButtonKivon= findViewById(R.id.ButtonMinus);
         Kimenet= findViewById(R.id.TextViewOutput);
+        Szam = 0;
 
 
 
