d64dc9a5f36094b21787c2a1d15773c33fca5ace
Ecsedi Milan <milan200314@gmail.com>
2022-09-23 11:34:10 +0200

    main.java   -buttonhozzaad added(but its not working :D )

--- app/src/main/java/com/example/clickcounter_dolgozat/MainActivity.java
+++ app/src/main/java/com/example/clickcounter_dolgozat/MainActivity.java
@@ -3,12 +3,50 @@ package com.example.clickcounter_dolgozat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button ButtonHozzaad;
+    private Button ButtonKivon;
+    private TextView Kimenet;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+
+        ButtonHozzaad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               String Szam = String.valueOf(Kimenet);
+                Szam  += 1;
+                Kimenet.setText(Szam);
+
+
+
+            }
+        });
+
+
     }
+
+    public void init(){
+
+
+        ButtonHozzaad= findViewById(R.id.ButtonPlus);
+        ButtonKivon= findViewById(R.id.ButtonMinus);
+        Kimenet= findViewById(R.id.TextViewOutput);
+
+
+
+    }
+
 }
